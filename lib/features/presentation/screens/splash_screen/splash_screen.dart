5f4b948b9afa586ad4e0fc01dca5c5104bd5a2f0
zoh01 <adebayozoh@gmail.com>
2025-11-26 17:01:11 +0100

    New

--- lib/features/presentation/screens/splash_screen/splash_screen.dart
+++ lib/features/presentation/screens/splash_screen/splash_screen.dart
@@ -1,5 +1,5 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:news_portal/features/presentation/screens/home_screen/home_screen.dart';
 import 'package:news_portal/utils/constants/image_strings.dart';
