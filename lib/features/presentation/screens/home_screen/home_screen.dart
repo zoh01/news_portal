bd7ffa4837c52ae932388bb5f7c8e1553765a1b6
zoh01 <adebayozoh@gmail.com>
2025-12-03 20:45:44 +0100

    Home

--- lib/features/presentation/screens/home_screen/home_screen.dart
+++ lib/features/presentation/screens/home_screen/home_screen.dart
@@ -90,196 +90,194 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final dark = ZohHelperFunction.isDarkMode(context);
 
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'ZOH',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: ZohSizes.defaultSpace,
-                  fontWeight: FontWeight.bold,
-                  color: ZohColors.primaryColor,
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'ZOH',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: ZohSizes.defaultSpace,
+                fontWeight: FontWeight.bold,
+                color: ZohColors.primaryColor,
               ),
-              Text(
-                'ARTICLES',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: ZohSizes.defaultSpace,
-                  fontWeight: FontWeight.bold,
-                  color: ZohColors.secondaryColor,
-                ),
+            ),
+            Text(
+              'ARTICLES',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: ZohSizes.defaultSpace,
+                fontWeight: FontWeight.bold,
+                color: ZohColors.secondaryColor,
               ),
-            ],
-          ),
-          backgroundColor: Colors.black26,
-          elevation: 4,
+            ),
+          ],
         ),
-        backgroundColor: dark ? Colors.black : Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.only(bottom: ZohSizes.sm),
-          child: RefreshIndicator(
-            onRefresh: () async {
-              await getNews();
-              await getSliders();
-            },
-            child: ListView(
-              physics: AlwaysScrollableScrollPhysics(),
-              children: [
-                /// HOTTEST NEWS
-                Padding(
-                  padding: const EdgeInsets.only(top: ZohSizes.sm),
-                  child: TextWidget(
-                    title: 'Hottest News',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        CupertinoPageRoute(builder: (ctx) => SliderViewAll()),
-                      );
-                    },
-                  ),
+        backgroundColor: Colors.black26,
+        elevation: 4,
+      ),
+      backgroundColor: dark ? Colors.black : Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: ZohSizes.sm),
+        child: RefreshIndicator(
+          onRefresh: () async {
+            await getNews();
+            await getSliders();
+          },
+          child: ListView(
+            physics: AlwaysScrollableScrollPhysics(),
+            children: [
+              /// HOTTEST NEWS
+              Padding(
+                padding: const EdgeInsets.only(top: ZohSizes.sm),
+                child: TextWidget(
+                  title: 'Hottest News',
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(builder: (ctx) => SliderViewAll()),
+                    );
+                  },
                 ),
+              ),
 
-                /// SLIDER
-                if (sliderLoading)
-                  Shimmer.fromColors(
-                    baseColor: Colors.white,
-                    highlightColor: Colors.transparent,
-                    child: Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: ZohSizes.sm,
-                        vertical: ZohSizes.sm,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(ZohSizes.md),
-                      ),
-                      child: Material(
-                        elevation: 3,
-                        borderRadius: BorderRadius.circular(ZohSizes.md),
-                        color: Colors.white,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(
-                                  ZohSizes.md,
-                                ),
-                                child: Image(
-                                  image: AssetImage(ZohImages.reportNews),
-                                  fit: BoxFit.cover,
-                                  width:
-                                      ZohDeviceUtils.getScreenWidth(context) *
-                                      .8,
-                                  height:
-                                      ZohHelperFunction.screenHeight() * .17,
-                                ),
+              /// SLIDER
+              if (sliderLoading)
+                Shimmer.fromColors(
+                  baseColor: Colors.white,
+                  highlightColor: Colors.transparent,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: ZohSizes.sm,
+                      vertical: ZohSizes.sm,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(ZohSizes.md),
+                    ),
+                    child: Material(
+                      elevation: 3,
+                      borderRadius: BorderRadius.circular(ZohSizes.md),
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(
+                                ZohSizes.md,
                               ),
-                              SizedBox(height: ZohSizes.sm),
-                              Text(
-                                "sliders[index].newsTitle!",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontFamily: 'Roboto',
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: ZohSizes.md,
-                                  color: Colors.black,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 2,
-                                softWrap: true,
+                              child: Image(
+                                image: AssetImage(ZohImages.reportNews),
+                                fit: BoxFit.cover,
+                                width:
+                                    ZohDeviceUtils.getScreenWidth(context) *
+                                    .8,
+                                height:
+                                    ZohHelperFunction.screenHeight() * .17,
                               ),
-                            ],
-                          ),
+                            ),
+                            SizedBox(height: ZohSizes.sm),
+                            Text(
+                              "sliders[index].newsTitle!",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.bold,
+                                fontSize: ZohSizes.md,
+                                color: Colors.black,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              softWrap: true,
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                  )
-                else if (sliders.isNotEmpty)
-                  CarouselSlider.builder(
-                    itemCount: min(sliders.length, 5),
-                    itemBuilder: (context, index, realIndex) {
-                      final resImage = sliders[index].newsImage ?? '';
-                      final resText = sliders[index].newsTitle ?? 'Untitled';
-                      return SliderContainer(
-                        context: context,
-                        sliders: sliders,
-                        image: resImage,
-                        index: index,
-                        name: resText,
-                      );
-                    },
-                    options: CarouselOptions(
-                      height: ZohHelperFunction.screenHeight() * .35,
-                      autoPlay: true,
-                      enlargeCenterPage: true,
-                      enlargeStrategy: CenterPageEnlargeStrategy.height,
-                      onPageChanged: (index, reason) {
-                        setState(() => activeIndex = index);
-                      },
-                    ),
                   ),
-
-                const SizedBox(height: ZohSizes.sm),
-
-                /// INDICATOR
-                if (sliders.isNotEmpty)
-                  Center(
-                    child: AnimatedSmoothIndicator(
-                      activeIndex: activeIndex,
-                      count: sliders.length > 5 ? 5 : sliders.length,
-                      effect: SlideEffect(
-                        activeDotColor: ZohColors.primaryColor,
-                        dotWidth: ZohSizes.spaceBtwZoh,
-                        dotHeight: ZohSizes.sm,
-                      ),
-                    ),
-                  ),
-
-                const SizedBox(height: ZohSizes.md),
-
-                /// EXPLORE
-                Padding(
-                  padding: const EdgeInsets.only(left: ZohSizes.sm),
-                  child: Text(
-                    'Explore',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: ZohSizes.spaceBtwZoh,
-                      fontWeight: FontWeight.bold,
-                      color: dark ? Colors.white : Colors.black,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: ZohSizes.sm),
-
-                ExploreCategories(categories: categories, shimmer: shimmer),
-
-                /// TRENDING NEWS
-                TextWidget(
-                  title: 'Trending News',
-                  onTap: () {
-                    Navigator.push(context, CupertinoPageRoute(builder: (zoh) => TrendingViewAll()));
+                )
+              else if (sliders.isNotEmpty)
+                CarouselSlider.builder(
+                  itemCount: min(sliders.length, 5),
+                  itemBuilder: (context, index, realIndex) {
+                    final resImage = sliders[index].newsImage ?? '';
+                    final resText = sliders[index].newsTitle ?? 'Untitled';
+                    return SliderContainer(
+                      context: context,
+                      sliders: sliders,
+                      image: resImage,
+                      index: index,
+                      name: resText,
+                    );
                   },
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: ZohSizes.sm),
-                  child: TrendingNews(
-                    trendingShimmer: newsLoading,
-                    articles: articles,
+                  options: CarouselOptions(
+                    height: ZohHelperFunction.screenHeight() * .35,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    enlargeStrategy: CenterPageEnlargeStrategy.height,
+                    onPageChanged: (index, reason) {
+                      setState(() => activeIndex = index);
+                    },
                   ),
                 ),
-              ],
-            ),
+
+              const SizedBox(height: ZohSizes.sm),
+
+              /// INDICATOR
+              if (sliders.isNotEmpty)
+                Center(
+                  child: AnimatedSmoothIndicator(
+                    activeIndex: activeIndex,
+                    count: sliders.length > 5 ? 5 : sliders.length,
+                    effect: SlideEffect(
+                      activeDotColor: ZohColors.primaryColor,
+                      dotWidth: ZohSizes.spaceBtwZoh,
+                      dotHeight: ZohSizes.sm,
+                    ),
+                  ),
+                ),
+
+              const SizedBox(height: ZohSizes.md),
+
+              /// EXPLORE
+              Padding(
+                padding: const EdgeInsets.only(left: ZohSizes.sm),
+                child: Text(
+                  'Explore',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: ZohSizes.spaceBtwZoh,
+                    fontWeight: FontWeight.bold,
+                    color: dark ? Colors.white : Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(height: ZohSizes.sm),
+
+              ExploreCategories(categories: categories, shimmer: shimmer),
+
+              /// TRENDING NEWS
+              TextWidget(
+                title: 'Trending News',
+                onTap: () {
+                  Navigator.push(context, CupertinoPageRoute(builder: (zoh) => TrendingViewAll()));
+                },
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: ZohSizes.sm),
+                child: TrendingNews(
+                  trendingShimmer: newsLoading,
+                  articles: articles,
+                ),
+              ),
+            ],
           ),
         ),
       ),
